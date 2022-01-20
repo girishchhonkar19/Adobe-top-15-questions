fd6c62b942d9f93870ff436a41e0e6c372dc02eb
Girish Kumar <85754888+girishchhonkar19@users.noreply.github.com>
2022-01-20 14:05:06 +0530

    Added solution - LeetHub

---
+++ Winner of an election - GFG/winner-of-an-election.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cstdlib>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+        vector<string>res;
+        map<string,int>m;
+        for(int i=0;i<n;i++)
+        m[arr[i]]++;
+        
+        int ans=0;
+        for(auto x:m)
+        {
+           if(x.second>ans)
+           ans=x.second;
+           
+        }
+        for(auto x:m)
+        {
+            if(x.second==ans)
+            {
+                res.push_back(x.first);
+            res.push_back(to_string(ans));
+            return res;
+            }
+            
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    for(int i=0;i<t;i++)
+    {
+        
+        
+        int n;
+        cin>>n;
+        
+        string arr[n];
+        
+        for (int i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        vector<string> result = obj.winner(arr,n);
+        
+        cout<<result[0] << " " << result[1] << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
